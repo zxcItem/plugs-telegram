19dc73f6f765ddfc23d191d0c4233e1ce76983b0
Mr zhou <48907124+zxcItem@users.noreply.github.com>
2025-05-07 14:52:06 +0800

    Update Data.php

--- src/controller/api/Data.php
+++ src/controller/api/Data.php
@@ -6,6 +6,7 @@ namespace plugin\telegram\controller\api;
 
 use plugin\telegram\service\TelegramApi;
 use think\admin\Controller;
+use think\admin\Storage;
 use think\exception\HttpResponseException;
 
 /**
@@ -28,6 +29,29 @@ class Data extends Controller
             throw $exception;
         } catch (\Exception $exception) {
             $this->error($exception->getMessage());
+        }
+    }
+
+    /**
+     * 二进制文件上传
+     * @throws \think\admin\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function upload()
+    {
+        $file = $this->request->file('file');
+        if (empty($file)) $this->error('文件上传异常！');
+        $extension = strtolower($file->getOriginalExtension());
+        if (in_array($extension, ['php', 'sh'])) $this->error('禁止上传此类文件！');
+        $bina = file_get_contents($file->getRealPath());
+        $name = Storage::name($file->getPathname(), $extension, '', 'md5_file');
+        $info = Storage::instance()->set($name, $bina, false, $file->getOriginalName());
+        if (is_array($info) && isset($info['url'])) {
+            $this->success('文件上传成功！', $info);
+        } else {
+            $this->error('文件上传失败！');
         }
     }
 }
